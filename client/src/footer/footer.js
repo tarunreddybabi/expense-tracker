ba2b62038b6545658beba425c174ad9216f7828c
tarunreddybabi <tarunreddybabi@gmail.com>
2024-03-05 15:04:09 +0530

    footer

--- client/src/footer/footer.js
+++ client/src/footer/footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
       <br />
       <br />
       <p style={{ marginLeft:"350px", textAlign: "center", fontSize: "14px", color: "black" }}>
-        © 2024 My Website. All rights reserved.
+        © 2024 Expense Tracker. All rights reserved.
       </p>
     </footer>
   );
